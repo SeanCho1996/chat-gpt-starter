9e1617b2458ced01120e2b095184a96786aad90c
SeanCho1996 <SeanCho1996@github.com>
2023-03-14 03:41:36 +0000

    add index page

---
+++ app2.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, jsonify, request
+import config
+import openai
+import aiapi
+
+api_key = config.DevelopmentConfig.OPENAI_KEY
+openai.api_key = api_key
+
+def page_not_found(e):
+  return render_template('404.html'), 404
+
+
+app = Flask(__name__, static_folder = './source/docs', template_folder = './source/docs', static_url_path = '')
+app.config.from_object(config.config['development'])
+
+app.register_error_handler(404, page_not_found)
+
+@app.route('/', methods = ['POST', 'GET'])
+def index():
+    # if request.method == "POST":
+    #   prompt = request.form["prompt"]
+    #   answer = aiapi.GetResponse(prompt)
+    #   print(answer)
+    #   res = {"answer": answer}
+
+    #   return jsonify(res), 200
+    return render_template('index.html', **locals())
+
+# @app.route('/', methods = ['POST', 'GET'])
+# def index():
+#     if request.method == "POST":
+#       prompt = request.form["prompt"]
+#       answer = aiapi.GetResponse(prompt)
+#       print(answer)
+#       res = {"answer": answer}
+
+#       return jsonify(res), 200
+#     return render_template('index.html', **locals())
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='35365', debug=True)
